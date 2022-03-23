702f1875bf8da2f7d41467981b4d5514410d8911
KSkun <ks@ksmeow.moe>
2022-03-23 22:01:35 +0800

    feat (debug): implement print_stackframe

--- labcodes/lab1/kern/debug/kdebug.c
+++ labcodes/lab1/kern/debug/kdebug.c
@@ -254,42 +254,7 @@ read_eip(void) {
     return eip;
 }
 
-/* *
- * print_stackframe - print a list of the saved eip values from the nested 'call'
- * instructions that led to the current point of execution
- *
- * The x86 stack pointer, namely esp, points to the lowest location on the stack
- * that is currently in use. Everything below that location in stack is free. Pushing
- * a value onto the stack will invole decreasing the stack pointer and then writing
- * the value to the place that stack pointer pointes to. And popping a value do the
- * opposite.
- *
- * The ebp (base pointer) register, in contrast, is associated with the stack
- * primarily by software convention. On entry to a C function, the function's
- * prologue code normally saves the previous function's base pointer by pushing
- * it onto the stack, and then copies the current esp value into ebp for the duration
- * of the function. If all the functions in a program obey this convention,
- * then at any given point during the program's execution, it is possible to trace
- * back through the stack by following the chain of saved ebp pointers and determining
- * exactly what nested sequence of function calls caused this particular point in the
- * program to be reached. This capability can be particularly useful, for example,
- * when a particular function causes an assert failure or panic because bad arguments
- * were passed to it, but you aren't sure who passed the bad arguments. A stack
- * backtrace lets you find the offending function.
- *
- * The inline function read_ebp() can tell us the value of current ebp. And the
- * non-inline function read_eip() is useful, it can read the value of current eip,
- * since while calling this function, read_eip() can read the caller's eip from
- * stack easily.
- *
- * In print_debuginfo(), the function debuginfo_eip() can get enough information about
- * calling-chain. Finally print_stackframe() will trace and print them for debugging.
- *
- * Note that, the length of ebp-chain is limited. In boot/bootasm.S, before jumping
- * to the kernel entry, the value of ebp has been set to zero, that's the boundary.
- * */
-void
-print_stackframe(void) {
+void print_stackframe(void) {
      /* LAB1 YOUR CODE : STEP 1 */
      /* (1) call read_ebp() to get the value of ebp. the type is (uint32_t);
       * (2) call read_eip() to get the value of eip. the type is (uint32_t);
@@ -302,5 +267,26 @@ print_stackframe(void) {
       *           NOTICE: the calling funciton's return addr eip  = ss:[ebp+4]
       *                   the calling funciton's ebp = ss:[ebp]
       */
+     uint32_t ebpVal = read_ebp(), eipVal = read_eip();
+     for (int i = 0; i < STACKFRAME_DEPTH; i++) {
+         if (ebpVal == 0) break;
+
+         // print ebp & eip
+         cprintf("ebp:0x%08x eip: 0x%08x args:", ebpVal, eipVal);
+
+         // print arguments
+         uint32_t *args = (uint32_t *) ebpVal + 2;
+         for (int j = 0; j < 4; j++) {
+             cprintf("0x%08x ", args[j]);
+         }
+         cprintf("\n");
+
+         // print function name & line number
+         print_debuginfo(eipVal - 1);
+
+         // pop a stack frame
+         ebpVal = *((uint32_t *) ebpVal);
+         eipVal = *((uint32_t *) ebpVal + 1);
+     }
 }
 
